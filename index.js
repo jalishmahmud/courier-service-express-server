be92fab9c467a008f5fb4b65f51a126856158864
jalishmahmud <jalish93@gmail.com>
2022-01-07 21:34:44 +0600

    personalInof post done

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//  DB user : JMCourier
+// DB pass: BhxZuaBAPjpetr2q
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.weui7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("JMCourier");
+    const userInformationCollection = database.collection("user-information");
+
+    // POST user information
+    app.post("/userInformation", async (req, res) => {
+      const userInformation = req.body;
+      const result = await userInformationCollection.insertOne(userInformation);
+      res.json(result);
+      console.log(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+require("dotenv").config();
+app.get("/", (req, res) => {
+  res.send("Hello I am ready! What is up?");
+});
+
+app.listen(port, () => {
+  console.log("Listening at port :", port);
+});
