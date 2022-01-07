e3bb56d00fdf063d613d08d38ba7ef154b963d18
jalishmahmud <jalish93@gmail.com>
2022-01-07 23:37:29 +0600

    testing git

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-//  DB user : JMCourier
-// DB pass: BhxZuaBAPjpetr2q
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.weui7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -25,7 +22,7 @@ async function run() {
     const database = client.db("JMCourier");
     const userInformationCollection = database.collection("user-information");
 
-    // POST user information
+    // POST user information API
     app.post("/userInformation", async (req, res) => {
       const userInformation = req.body;
       const result = await userInformationCollection.insertOne(userInformation);
