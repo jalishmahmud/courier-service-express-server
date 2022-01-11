65a44e9ceb69dbbafdf8765fdfa850e79ad5e41d
jalishmahmud <jalish93@gmail.com>
2022-01-11 10:56:44 +0600

    area, district, upjela API done

--- index.js
+++ index.js
@@ -21,14 +21,64 @@ async function run() {
     await client.connect();
     const database = client.db("JMCourier");
     const userInformationCollection = database.collection("user-information");
-
+    const areaCollection = database.collection("area");
+    const districtCollection = database.collection("district");
+    const upojelaCollection = database.collection("upojela");
+    const locationCollection = database.collection("location");
+    // userInformationCollection =======================================
     // POST user information API
     app.post("/userInformation", async (req, res) => {
       const userInformation = req.body;
       const result = await userInformationCollection.insertOne(userInformation);
       res.json(result);
-      console.log(result);
     });
+
+    // configuration ============================================
+    // POST new area
+    app.post("/addNewArea", async (req, res) => {
+      const userInformation = req.body;
+      const result = await areaCollection.insertOne(userInformation);
+      res.json(result);
+    });
+    // GET all aria
+    app.get("/getAllArea", async (req, res) => {
+      const cursor = areaCollection.find({});
+      const areas = await cursor.toArray();
+      res.json(areas);
+    });
+    // POST new District
+    app.post("/addNewDistrict", async (req, res) => {
+      const userInformation = req.body;
+      const result = await districtCollection.insertOne(userInformation);
+      res.json(result);
+    });
+    // GET all district
+    app.get("/getAllDistrict", async (req, res) => {
+      const cursor = districtCollection.find({});
+      const district = await cursor.toArray();
+      res.json(district);
+    });
+    // POST new Upojela
+    app.post("/addNewUpojela", async (req, res) => {
+      const userInformation = req.body;
+      const result = await upojelaCollection.insertOne(userInformation);
+      res.json(result);
+    });
+    // GET new Upojela
+    app.get("/getAllUpojela", async (req, res) => {
+      const cursor = upojelaCollection.find({});
+      const upojela = await cursor.toArray();
+      res.json(upojela);
+    });
+
+    // POST new Location
+    app.post("/addNewLocation", async (req, res) => {
+      const userInformation = req.body;
+      const result = await locationCollection.insertOne(userInformation);
+      res.json(result);
+    });
+
+    //=============================================================
   } finally {
     // await client.close();
   }
